b43e704035902e669f75c28fb8567b01b1885a03
matthewmorse0 <matthewmorse0@gmail.com>
2021-03-31 16:47:25 -0500

    Updating account creation

--- OOP_MainProject/CreateAccount.java
+++ OOP_MainProject/CreateAccount.java
@@ -1,20 +1,78 @@
 package OOP_MainProject;
 
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 
 public class CreateAccount {
-    public static void accountCreate(){
+    void accountCreate(){
         Scanner keyboardInput = new Scanner(System.in);
-        String password, name, address, phone, CC, ID;
-        System.out.println("Please enter a user ID:");
-        ID = keyboardInput.nextLine();
-        System.out.println("Please enter your name:");
-        name = keyboardInput.nextLine();
-        System.out.println("Please enter your address:");
-        address = keyboardInput.nextLine();
-        System.out.println("Please enter your phone number:");
-        phone = keyboardInput.nextLine();
-        System.out.println("Please enter your credit card number:");
-        CC = keyboardInput.nextLine();
+        boolean loopDone = true;
+        String password, name, address, phone, CC, ID, accType, fileLine;
+
+        //Create file if necessary
+        File accountsFile = new File("accounts.txt");
+        try {
+            //WARNING: I'm not sure if this will create a new file every time. Check later
+            if (accountsFile.createNewFile()) {
+                System.out.println("File created: " + accountsFile.getName());
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        while (true) {
+            System.out.println("Please enter a user ID:");
+            ID = keyboardInput.nextLine();
+            System.out.println("Please enter a password:");
+            password = keyboardInput.nextLine();
+            //Check if account ID already exists
+            try {
+                Scanner accountsReader = new Scanner(accountsFile);
+                while (accountsReader.hasNextLine()) {
+                    fileLine = accountsReader.nextLine();
+                    if (fileLine.equals(ID)) {
+                        System.out.println("This ID is already registered. Please try again.");
+                        loopDone = false;
+                        break;
+                    }
+                }
+                if (loopDone == true){
+                    continue;
+                }
+                accountsReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+
+            System.out.println("Please enter your name:");
+            name = keyboardInput.nextLine();
+            System.out.println("Please enter your address:");
+            address = keyboardInput.nextLine();
+            System.out.println("Please enter your phone number:");
+            phone = keyboardInput.nextLine();
+            System.out.println("Please enter your credit card number:");
+            CC = keyboardInput.nextLine();
+            System.out.println("Please select a premium ($40) or a regular membership by entering (premium/regular)");
+            accType = keyboardInput.nextLine();
+            //TODO: Charge $40 when premium selected
+
+            //Write user information to file
+            try {
+                FileWriter writer = new FileWriter("accounts.txt");
+                writer.write(ID + "\n" + name + "\n" + address + "\n" + phone + "\n" + CC + "\n" + accType + "\n\n");
+                writer.close();
+            } catch (IOException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+            break;
+        }
+
+
     }
 }
--- OOP_MainProject/Main.java
+++ OOP_MainProject/Main.java
@@ -1,13 +1,30 @@
 package OOP_MainProject;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        int choice;
+        Scanner keyboardInput = new Scanner(System.in);
+        CreateAccount acc = new CreateAccount();
 	// changes to update repository
         //add somethingS
         //Hey, it's me
         //Hola papi
         Menu test = new testClass();
         test.testMethod();
+        System.out.println("Please select an option: \n1) Log in\n2)Create Account");
+        choice = keyboardInput.nextInt();
+        switch(choice){
+            case 1:
+                System.out.println("You will log in here...");
+                break;
+            case 2:
+                System.out.println("Directing you to account creation...");
+                acc.accountCreate();
+                break;
+        }
+
     }
 }
