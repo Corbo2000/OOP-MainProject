54aefc6379736aee899109f7a0ebb58904e97ac4
matthewmorse0 <matthewmorse0@gmail.com>
2021-04-07 15:56:25 -0500

    Better notation

--- MainProjectPackage/HandleOrder.java
+++ MainProjectPackage/HandleOrder.java
@@ -12,10 +12,8 @@ public class HandleOrder {
         File items = new File("ItemCatalog.txt");
         List<String> itemList = new ArrayList<String>();
         List<Integer> itemQuantity = new ArrayList<Integer>();
-        List<String> selectedItems = new ArrayList<String>();
-        String itemName, itemDesc;
         String[] itemInfo = {"", "", "", ""};
-        float regPrice, premPrice, totalPrice = 0;
+        float totalPrice = 0;
         int counter1 = 0, counter2 = 0, choice = -2;
 
         try {
@@ -42,8 +40,10 @@ public class HandleOrder {
             System.out.println("\nPlease select an item to add to cart. Enter 0 to confirm, or -1 to exit");
             choice = keyboard.nextInt();
             if (choice == -1){
+                //Exit
                 break;
             }else if (choice == 0){
+                //Process order
                 counter1 = itemQuantity.size();
                 counter2 = 0;
                 System.out.println("The following is currently in your cart:");
@@ -61,14 +61,10 @@ public class HandleOrder {
             }
             counter1++;
             itemInfo = itemList.get(choice - 1).split(",", 4);
-            System.out.println("The selected item " + itemInfo[0] + ": " + itemInfo[1]);
-            System.out.println("Regular price: " + itemInfo[2] + "\nPremium Price: " + itemInfo[3]);
             if (userType.equals("regular")){
                 itemQuantity.set(choice - 1, itemQuantity.get(choice - 1) + 1);
                 totalPrice += Float.parseFloat(itemInfo[2]);
-                //selectedItems.add(itemInfo[0] + itemInfo[2]);
             }else{
-                //selectedItems.add(itemInfo[0] + itemInfo[3]);
                 itemQuantity.set(choice - 1, itemQuantity.get(choice - 1) + 1);
                 totalPrice += Float.parseFloat(itemInfo[3]);
             }
