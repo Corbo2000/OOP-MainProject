17567c22e790e71535b367ccbe07bad25002a9de
DonutsWithDad <colbin.hobbs@ttu.edu>
2021-04-18 01:59:20 -0500

    JavaFX Changed
    I made advancements on JavaFX, the login is now in JavaFX, but the login does not work. Work on taking the TextFields and passing them to log() to login with.

--- MainProjectPackage/Main.java
+++ MainProjectPackage/Main.java
@@ -17,53 +17,80 @@ public class Main extends Application{
 
     private Label selectionLabel;
 
+    // For the sake of simplicity, we have only one window and multiple scenes
+    // all this needs to do is take in data and put it into is respective classes/methods
+    Stage window;
+    Scene menuScene, LoginScene, CNAScene, SelectItemsScene, MakeOrderScene, ViewInvoiceScene, ViewOrderScene; // used by customer
+    Scene ProcessOrderScene, ShipOrderScene, ViewStockScene;
+
+    int i = 0;
     public static void main(String[] args) {
         launch(args);
-        /*int choice;
-        String userInfo;
-        Scanner keyboardInput = new Scanner(System.in);
-        CreateAccount acc = new CreateAccount();
-        LogIn login = new LogIn();
-            System.out.println("Please select an option: \n1) Log in\n2) Create Account");
-            choice = keyboardInput.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.println("You will log in here...");
-                    login.log();
-                    break;
-                case 2:
-                    System.out.println("Directing you to account creation...");
-                    acc.accountCreate();
-                    break;
-            }*/
         }
         @Override
         public void start(Stage primaryStage){
-            Label selectionLabel = new Label("Select Login or Create New Account");
-            Button loginButton = new Button("Login");
-            Button cnaButton = new Button("Create New Account");
+            // our initial window
+            window = primaryStage;
 
-            loginButton.setOnAction(new LoginHandler());
-            cnaButton.setOnAction(new cnaHandler());
+            //Scene: MenuScene
+            {
+                Label selectionLabel = new Label("Select Login or Create New Account");
+                Button loginButton = new Button("Login");
+                Button cnaButton = new Button("Create New Account");
+                Button exit = new Button("Exit");
 
-            VBox menu = new VBox(20, selectionLabel, loginButton, cnaButton);
-            menu.setAlignment(Pos.CENTER);
-            menu.setPadding(new Insets(25));
+                loginButton.setOnAction(new LoginHandler());
+                cnaButton.setOnAction(new cnaHandler());
+                exit.setOnAction(new exitHandler());
 
-            Scene menuScene = new Scene(menu);
-            primaryStage.setScene(menuScene);
-            primaryStage.setTitle("Menu");
-            primaryStage.show();
+                VBox menu = new VBox(20, selectionLabel, loginButton, cnaButton, exit);
+                menu.setAlignment(Pos.CENTER);
+                menu.setPadding(new Insets(25));
+
+                menuScene = new Scene(menu);
+
+                // initial set scene
+                window.setScene(menuScene);
+                window.setTitle("Online Shopping System");
+                window.show();
+            }
         }
 
         class LoginHandler implements EventHandler<ActionEvent> {
             @Override
             public void handle(ActionEvent actionEvent) {
-
+                /*
                 LogIn login = new LogIn();
 
                 login.log();
+                */
+                //Label Creation
+                Label UsernamePrompt = new Label("Enter your username: ");
+                Label PasswordPrompt = new Label("Enter your password: ");
 
+                //TextField Creation
+                TextField username = new TextField();
+                TextField password = new TextField();
+
+                HBox H_user = new HBox(20,UsernamePrompt, username);
+                HBox P_user = new HBox(20,PasswordPrompt, password);
+
+                //Button creation
+                Button confirmLogin = new Button("Login");
+                Button exit = new Button("Exit");
+
+                VBox HP_user = new VBox(20,H_user, P_user, confirmLogin, exit);
+                HP_user.setAlignment(Pos.CENTER);
+                HP_user.setPadding(new Insets(25));
+
+                //Actions
+                confirmLogin.setOnAction(new LoginHandler());
+
+                LoginScene = new Scene(HP_user);
+
+                window.setScene(LoginScene);
+                window.setTitle("Login");
+                window.show();
             }
         }
 
@@ -75,6 +102,13 @@ public class Main extends Application{
 
             acc.accountCreate();
 
+        }
+    }
+    class exitHandler implements EventHandler<ActionEvent>{
+
+        @Override
+        public void handle(ActionEvent actionEvent) {
+            System.exit(0);
         }
     }
 
